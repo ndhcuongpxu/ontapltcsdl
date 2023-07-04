73c3ccdb82342e2d529a7b00101fa3adfbad9a1a
ndhcuongpxu <ndhcuong.hue2023@gmail.com>
2023-07-04 14:22:48 +0700

    thay doi lan 2

--- src/pxu/edu/vn/nhapsv/StudentModel.java
+++ src/pxu/edu/vn/nhapsv/StudentModel.java
@@ -1,5 +1,52 @@
 package pxu.edu.vn.nhapsv;
 
-public class StudentModel {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.text.SimpleDateFormat;
 
+public class StudentModel {
+	static Connection conn;
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+	public StudentModel() {
+		conn = connnectDB();
+	}
+
+	public void addStudent(Student input) {
+		PreparedStatement ps = null;
+		try {
+
+			String sql = "insert into  student(hoten,ngaysinh,gioitinh,sdt) values(?,?,?,?)";
+
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1, input.getHoten());
+			statement.setString(2, dateFormat.format(input.getNgaysinh()));
+			statement.setString(3, input.getGioitinh());
+			if (input.getSdt() != null) {
+				statement.setString(4, input.getSdt());
+			}
+			// Step 6: Process the results
+			ps.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Connection connnectDB() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/qlsv";
+			String userName = "root";
+			String password = "@Dmin1234";
+			Class.forName("com.mysql.jdbc.Driver");
+			if (conn == null) {
+				conn = DriverManager.getConnection(dbURL, userName, password);
+				System.out.println("connect successfully!");
+			}
+		} catch (Exception ex) {
+			System.out.println("connect failure!");
+			ex.printStackTrace();
+		}
+		return conn;
+	}
 }
--- src/pxu/edu/vn/nhapsv/StudentView.java
+++ src/pxu/edu/vn/nhapsv/StudentView.java
@@ -1,5 +1,44 @@
 package pxu.edu.vn.nhapsv;
 
-public class StudentView {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
+public class StudentView {
+	private Scanner sc;
+public void nhapsvView() {
+		sc = new Scanner(System.in);
+		
+}
+public Student NhapSV() throws ParseException {
+	Student student = new Student();
+	System.out.println("Nhap ID sinh vien: ");
+   int idSV = sc.nextInt();
+   student.setIdstudent(idSV);
+   
+   
+   System.out.println("Nhap ho ten sinh vien: ");
+   String hotenSV = sc.next();
+   student.setHoten(hotenSV);
+   
+   
+   System.out.println("Nhap ngay sinh: ");
+   String ngaysinhSV = sc.next();
+   SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+   Date ngaySinh = dateFormat.parse(ngaysinhSV);
+   student.setNgaysinh(ngaySinh);
+   
+   System.out.println("Nhap gioi tinh: ");
+   String gioitinhSV = sc.next();
+   student.setGioitinh(gioitinhSV);
+   
+   System.out.println("Nhap sdt: ");
+   String sdtSV =sc.next();
+   student.setSdt(sdtSV);
+	return student;
+	
+}
+public void hienThiThongBao() {
+	System.out.println("Ban da nhap thanh cong sinh vien!!!");
+}
 }
